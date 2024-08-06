4c6d6315fd5e45c3753b5aa8a9dc0c773d836978
ingobeans <127751491+ingobeans@users.noreply.github.com>
2024-08-06 10:37:35 +0200

    simplified code

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,11 @@
 use std::fs;
 use std::process::Command;
 
-fn main() -> std::io::Result<()> {
-    let dir_path = "startup";
+fn main() {
+    let paths = fs::read_dir("startup").unwrap();
 
-    let entries = fs::read_dir(dir_path)?;
-
-    for entry in entries {
-        let entry = entry?;
-        let path = entry.path();
-
-        if path.is_file() {
-            Command::new("cmd").args(&["/C", "start", "", path.to_str().unwrap()]).spawn()?;
-        }
+    for entry in paths {
+        let path = entry.unwrap().path();
+        Command::new("cmd").args(["/C", "start", path.to_str().unwrap()]).spawn().unwrap();
     }
-
-    Ok(())
 }
